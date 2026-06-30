288efce8a1ec799c96ba78fb75fbb2dac5c1bb79
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-06-30 15:50:29 +0330

    IMPLEMENT_METHOD: 'constructors' implemented

--- PlaTwo/core/player.cpp
+++ PlaTwo/core/player.cpp
@@ -1,3 +1,17 @@
 #include "player.h"
 
-Player::Player() {}
+Player::Player():
+    isActive(true),
+    createdAt(QDateTime::currentDateTime())
+{}
+
+Player::Player(const QString& _username, const QString& _password,
+       const QString& _name, const QString& _phoneNumber, const QString& _email):
+    username(_username),
+    password(_password),
+    name(_name),
+    phoneNumber(_phoneNumber),
+    email(_email),
+    isActive(true),
+    createdAt(QDateTime::currentDateTime())
+{}
