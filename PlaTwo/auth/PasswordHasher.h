c02c8b7c959e6415a1cb3fd8f72658e09eaa860f
Erfan <erfansorouri1390@gmail.com>
2026-07-01 17:33:35 +0330

    ADD_FILE:auth folder completed

--- PlaTwo/auth/PasswordHasher.h
+++ PlaTwo/auth/PasswordHasher.h
@@ -1,10 +1,13 @@
 #ifndef PASSWORDHASHER_H
 #define PASSWORDHASHER_H
-
+#include <QString>
+#include <QCryptographicHash>
 class PasswordHasher
 {
 public:
-    PasswordHasher();
+    static QString hashPassword(const QString& password);
+    static bool verifyPassword(const QString& password, const QString& hash);
+    static QString generateSalt();
 };
 
 #endif // PASSWORDHASHER_H
