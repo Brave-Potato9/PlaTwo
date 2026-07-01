b8cb1fff83d762842056e1e80e8c0259ea7c4ea9
Erfanss1405 <141412738+Erfanuser85@users.noreply.github.com>
2026-07-01 17:21:00 +0330

    Update AuthManager.h

--- PlaTwo/auth/AuthManager.h
+++ PlaTwo/auth/AuthManager.h
@@ -1,10 +1,47 @@
 #ifndef AUTHMANAGER_H
 #define AUTHMANAGER_H
-
-class AuthManager
+#include "PlayerManager.h"
+#include "PasswordHasher.h"
+#include <QObject>
+class AuthManager : public QObject
 {
+    Q_OBJECT
+private:
+    PlayerManager playerManager;
+    QString currentPlayer;
+
+    //check_phone_email_password
+    bool validatePhone(const QString& phone) const;
+    bool validateEmail(const QString& email) const;
+    bool validatePassword(const QString& password) const;
+
 public:
-    AuthManager();
+    //constructor
+    explicit AuthManager(QObject * parent = nullptr);
+
+    //signup_login_logout
+    bool signup(const QString& username, const QString& password, const QString& name, const QString& phone, const QString& email);
+    bool login(const QString& username, const QString& password);
+    bool isLoggedIn(const QString& username) const ;
+    bool logout(const QString& username);
+
+    //change_and_reset_password
+    bool changePassword(const QString& username, const QString& oldPassword, const QString& newPassword);
+    bool resetPassword(const QString& phone, const QString& newPassword);
+
+    //get_current_player_and_data
+    QString getCurrentPlayer() const {return currentPlayer;}
+    Player getCurrentPlayerData() const;
+
+signals:
+    void loginSuccess(const QString& username);
+    void loginFailed(const QString& reason);
+    void signupSuccess(const QString& username);
+    void signupFailed(const QString& reason);
+    void logoutSuccess(const QString& username);
+    void passwordChanged(const QString& username);
+
+
 };
 
 #endif // AUTHMANAGER_H
