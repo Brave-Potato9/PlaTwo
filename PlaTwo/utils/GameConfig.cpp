644e631cf17cab5884653aa07c1ec234a3e34030
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-01 15:46:44 +0330

    IMPLEMENT_METHOD: 'setters' implemented

--- PlaTwo/utils/GameConfig.cpp
+++ PlaTwo/utils/GameConfig.cpp
@@ -54,3 +54,65 @@ bool GameConfig::getUseFlyingPhase() const
 {
     return useFlyingPhase;
 }
+
+//------------------------------------ setters ------------------------------------
+void GameConfig::setGameType(const QString& _gameType)
+{
+    if (_gameType == "DotsAndBoxes" ||
+        _gameType == "Morris" ||
+        _gameType == "Fanorona")
+    {
+        gameType = _gameType;
+    }
+    else
+    {
+        gameType = "Unknown";
+    }
+}
+
+void GameConfig::setHasTimeLimit(bool _hasTimeLimit)
+{
+    hasTimeLimit = _hasTimeLimit;
+    validateAndFix();
+}
+
+void GameConfig::setTimeLimit(int _timeLimit)
+{
+    timeLimit = _timeLimit;
+    validateAndFix();
+}
+
+void GameConfig::setServerPort(int _serverPort)
+{
+    serverPort = _serverPort;
+    validateAndFix();
+}
+
+void GameConfig::setServerIP(const QString& _serverIP)
+{
+    serverIP = _serverIP.isEmpty() ? "127.0.0.1" : _serverIP;
+}
+
+void GameConfig::setDotsBoardSize(int _rows, int _columns)
+{
+    dotsAndBoxesRows = _rows;
+    dotsAndBoxesColumns = _columns;
+    validateAndFix();
+}
+
+void GameConfig::setDotsAndBoxesRows(int _dotsAndBoxesRows)
+{
+    dotsAndBoxesRows = _dotsAndBoxesRows;
+    validateAndFix();
+}
+
+void GameConfig::setDotsAndBoxesColumns(int _dotsAndBoxesColumns)
+{
+    dotsAndBoxesColumns = _dotsAndBoxesColumns;
+    validateAndFix();
+}
+
+void GameConfig::setUseFlyingPhase(bool _useFlyingPhase)
+{
+    useFlyingPhase = _useFlyingPhase;
+}
