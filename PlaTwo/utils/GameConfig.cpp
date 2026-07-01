d22f56cbdb4a53c8db3fa36e6eb85ab680fc56f6
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-01 15:44:11 +0330

    IMPLEMENT_METHOD: 'getters' implemented

--- PlaTwo/utils/GameConfig.cpp
+++ PlaTwo/utils/GameConfig.cpp
@@ -13,3 +13,44 @@ GameConfig::GameConfig()
 {
     validateAndFix();
 }
+
+//------------------------------------ getters ------------------------------------
+QString GameConfig::getGameType() const
+{
+    return gameType;
+}
+
+bool GameConfig::getHasTimeLimit() const
+{
+    return hasTimeLimit;
+}
+
+int GameConfig::getTimeLimit() const
+{
+    return timeLimit;
+}
+
+int GameConfig::getServerPort() const
+{
+    return serverPort;
+}
+
+QString GameConfig::getServerIP() const
+{
+    return serverIP;
+}
+
+int GameConfig::getDotsAndBoxesRows() const
+{
+    return dotsAndBoxesRows;
+}
+
+int GameConfig::getDotsAndBoxesColumns() const
+{
+    return dotsAndBoxesColumns;
+}
+
+bool GameConfig::getUseFlyingPhase() const
+{
+    return useFlyingPhase;
+}
