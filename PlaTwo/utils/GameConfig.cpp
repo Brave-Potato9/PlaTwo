d4d3a9ae70ad72af0d33c5114e2dfa234303935c
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-01 15:42:48 +0330

    IMPLEMENT_METHOD: 'constructors' implemented

--- PlaTwo/utils/GameConfig.cpp
+++ PlaTwo/utils/GameConfig.cpp
@@ -1,3 +1,15 @@
 #include "GameConfig.h"
 
-GameConfig::GameConfig() {}
+//------------------------------------ constructor ------------------------------------
+GameConfig::GameConfig()
+    : gameType("Unknown")
+    , hasTimeLimit(false)
+    , timeLimit(60)
+    , serverPort(12345)
+    , serverIP("127.0.0.1")
+    , dotsAndBoxesRows(5)
+    , dotsAndBoxesColumns(5)
+    , useFlyingPhase(true)
+{
+    validateAndFix();
+}
