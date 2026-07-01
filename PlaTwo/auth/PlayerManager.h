c887af8611ba6815eb202462786a3351922887ce
Erfanss1405 <141412738+Erfanuser85@users.noreply.github.com>
2026-07-01 14:41:34 +0330

    Update PlayerManager.h

--- PlaTwo/auth/PlayerManager.h
+++ PlaTwo/auth/PlayerManager.h
@@ -1,12 +1,44 @@
 #ifndef PLAYERMANAGER_H
 #define PLAYERMANAGER_H
-
+#include <QList>
 #include <QObject>
-
-class PlayerManager
+#include <QString>
+#include "../core/Player.h"
+class PlayerManager : public QObject
 {
+    Q_OBJECT
+private:
+    QList<Player> players;
+    QString filePath;
 public:
-    PlayerManager();
+    //constructor
+    explicit PlayerManager(QObject * parent = nullptr);
+
+    //load_and_save_players
+    bool loadPlayers();
+    bool savePlayers();
+
+    //add_remove_update_player
+    bool addPlayer(const Player& player);
+    bool removePlayer(const QString& username);
+    bool updatePlayer(const Player& player);
+
+    //check_Exists_and_find
+    bool findPlayer(const QString& username, Player& foundedPlayer) const;
+    bool playerExists(const QString& username);
+
+    //get_all_and_active_players
+    QList<Player> getAllPlayers() const;
+    QList<Player> getActivePlayers() const;
+
+    //update_last_login
+    void updateLastLogin(const QString& username);
+
+signals:
+    void playerAdded(const QString& username);
+    void playerRemoved(const QString& username);
+    void playerUpdated(const QString& username);
+
 };
 
 #endif // PLAYERMANAGER_H
