62e7808cc4049b0e70261698c1998d8d29070d95
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-02 16:22:56 +0330

    IMPLEMENT_METHOD: 'destructors'/'score_management_methods' implemented

--- PlaTwo/core/Game.cpp
+++ PlaTwo/core/Game.cpp
@@ -8,4 +8,53 @@ Game::Game(QObject* parent)
     , scores(2, 0)
 {}
 
+//------------------------------------ constructors ------------------------------------
+Game::~Game(){}
+
+//------------------------------------ score_management_methods ------------------------------------
+QVector<int> Game::getScores() const
+{
+    return scores;
+}
+
+int Game::getScore(int playerIndex) const
+{
+    if (playerIndex < 0 || playerIndex >= scores.size())
+    {
+        return 0;
+    }
+
+    return scores[playerIndex];
+}
+
+void Game::addScore(int playerIndex, int points)
+{
+    if (playerIndex < 0 || playerIndex >= scores.size())
+    {
+        return;
+    }
+
+    scores[playerIndex] += points;
+    emit scoreUpdated(playerIndex, scores[playerIndex]);
+    emit scoresUpdated(scores);
+}
+
+void Game::setScore(int playerIndex, int points)
+{
+    if (playerIndex < 0 || playerIndex >= scores.size())
+    {
+        return;
+    }
+
+    scores[playerIndex] = points;
+    emit scoreUpdated(playerIndex, scores[playerIndex]);
+    emit scoresUpdated(scores);
+}
+
+void Game::clearScores()
+{
+    scores[0] = 0;
+    scores[1] = 0;
+    emit scoresUpdated(scores);
+}
 
