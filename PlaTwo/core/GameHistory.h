2f553abc05664e3d41b0489f1abb1caa112156f6
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-03 13:21:15 +0330

    ADD_CLASS: 'GameHistory'

--- PlaTwo/core/GameHistory.h
+++ PlaTwo/core/GameHistory.h
@@ -1,10 +1,54 @@
 #ifndef GAMEHISTORY_H
 #define GAMEHISTORY_H
 
+#include <QString>
+#include <QStringList>
+#include <QDateTime>
+#include <QVector>
+#include <QJsonObject>
+#include <QJsonArray>
+#include "Move.h"
+
 class GameHistory
 {
+private:
+    QStringList players;
+    QDateTime startTime;
+    QDateTime endTime;
+    QString winner;
+    QString gameType;
+    QVector<int> scores;
+    QList<Move> moves;
+
 public:
+    //constructor
     GameHistory();
+    GameHistory(const QStringList& players, const QString& gameType);
+
+    //getter
+    QStringList getPlayers() const ;
+    QDateTime getStartTime() const ;
+    QDateTime getEndTime() const ;
+    QString getWinner() const ;
+    QString getGameType() const ;
+    QVector<int> getScores() const ;
+    QList<Move> getMoves() const ;
+
+    //setter
+    void setPlayers(const QStringList& value) ;
+    void setStartTime(const QDateTime& value) ;
+    void setEndTime(const QDateTime& value) ;
+    void setWinner(const QString& value) ;
+    void setGameType(const QString& value) ;
+    void setScores(const QVector<int>& value) ;
+    void setMoves(const QList<Move>& value) ;
+
+    //misc
+    void addMove(const Move& move) ;
+
+    //working_with_JSON
+    QJsonObject toJson() const;
+    static GameHistory fromJson(const QJsonObject& obj);
 };
 
 #endif // GAMEHISTORY_H
