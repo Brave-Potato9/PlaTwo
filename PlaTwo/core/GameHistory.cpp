a5973c65b5266de071d6bab2af7fe99cdc4c113c
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-03 13:31:55 +0330

    IMPLEMENT_METHOD: 'getters' implemented

--- PlaTwo/core/GameHistory.cpp
+++ PlaTwo/core/GameHistory.cpp
@@ -10,3 +10,39 @@ GameHistory::GameHistory(const QStringList& players, const QString& gameType)
     , gameType(gameType)
     , scores(2, 0)
 {}
+
+//------------------------------------ getter ------------------------------------
+QStringList GameHistory::getPlayers() const
+{
+    return players;
+}
+
+QDateTime GameHistory::getStartTime() const
+{
+    return startTime;
+}
+
+QDateTime GameHistory::getEndTime() const
+{
+    return endTime;
+}
+
+QString GameHistory::getWinner() const
+{
+    return winner;
+}
+
+QString GameHistory::getGameType() const
+{
+    return gameType;
+}
+
+QVector<int> GameHistory::getScores() const
+{
+    return  scores;
+}
+
+QList<Move> GameHistory::getMoves() const
+{
+    return moves;
+}
