5383f278fb04c74818c007591d06625bc622078b
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-03 13:28:28 +0330

    IMPLEMENT_METHOD: 'constructors' implemented

--- PlaTwo/core/GameHistory.cpp
+++ PlaTwo/core/GameHistory.cpp
@@ -1,3 +1,12 @@
 #include "GameHistory.h"
 
-GameHistory::GameHistory() {}
+//------------------------------------ constructor ------------------------------------
+GameHistory::GameHistory():
+    scores(2, 0)
+{}
+
+GameHistory::GameHistory(const QStringList& players, const QString& gameType)
+    : players(players)
+    , gameType(gameType)
+    , scores(2, 0)
+{}
