855ac8c61f71d46ff22dd0b1ae4a7c769354ca8e
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-03 13:38:36 +0330

    IMPLEMENT_METHOD: 'setters' implemented

--- PlaTwo/core/GameHistory.cpp
+++ PlaTwo/core/GameHistory.cpp
@@ -46,3 +46,39 @@ QList<Move> GameHistory::getMoves() const
 {
     return moves;
 }
+
+//------------------------------------ getter ------------------------------------
+void GameHistory::setPlayers(const QStringList& _players)
+{
+    players = _players;
+}
+
+void GameHistory::setStartTime(const QDateTime& _startTime)
+{
+    startTime = _startTime;
+}
+
+void GameHistory::setEndTime(const QDateTime& _endTime)
+{
+    endTime - _endTime;
+}
+
+void GameHistory::setWinner(const QString& _winner)
+{
+    winner = _winner;
+}
+
+void GameHistory::setGameType(const QString& _gameType)
+{
+    gameType = _gameType;
+}
+
+void GameHistory::setScores(const QVector<int>& _scores)
+{
+    scores = _scores;
+}
+\
+void GameHistory::setMoves(const QList<Move>& _moves)
+{
+    moves = _moves;
+}
