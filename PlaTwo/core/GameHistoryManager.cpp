0e7aeca4724f0e52d657081da145f8f356a7f072
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-04 15:49:30 +0330

    IMPLEMENT_METHOD: 'constructors' / 'helper_methods' implemented

--- PlaTwo/core/GameHistoryManager.cpp
+++ PlaTwo/core/GameHistoryManager.cpp
@@ -1,3 +1,82 @@
-#include "HameHistoryManager.h"
+#include "GameHistoryManager.h"
+#include <QFile>
+#include <QDir>
+#include <QJsonDocument>
+#include <QJsonArray>
+#include <QJsonObject>
 
-GameHistoryManager::GameHistoryManager() {}
+//------------------------------------ constructor ------------------------------------
+GameHistoryManager::GameHistoryManager(QObject* parent)
+    : QObject(parent)
+{}
+
+//------------------------------------ helper_methods ------------------------------------
+QString GameHistoryManager::ensureDirectoryExists(const QString& username) const
+{
+    QString dirPath = "data/" + username;
+    QDir dir(dirPath);
+    if (!dir.exists())
+    {
+        dir.mkpath(".");
+    }
+
+    return dirPath;
+}
+
+QList<GameHistory> GameHistoryManager::readAllHistories(const QString& filePath) const
+{
+    QList<GameHistory> histories;
+
+    QFile file(filePath);
+    if (!file.exists())
+    {
+        return histories;
+    }
+
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        return histories;
+    }
+
+    QByteArray data = file.readAll();
+    file.close();
+
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    if (!doc.isArray())
+    {
+        return histories;
+    }
+
+    const QJsonArray array = doc.array();
+    for (const auto& item : array)
+    {
+        if (item.isObject())
+        {
+            GameHistory history = GameHistory::fromJson(item.toObject());
+            histories.append(history);
+        }
+    }
+
+    return histories;
+}
+
+bool GameHistoryManager::writeAllHistories(const QString& filePath, const QList<GameHistory>& histories) const
+{
+    QJsonArray array;
+    for (const GameHistory& history : histories)
+    {
+        array.append(history.toJson());
+    }
+
+    QJsonDocument doc(array);
+    QFile file(filePath);
+
+    if (!file.open(QIODevice::WriteOnly))
+    {
+        return false;
+    }
+
+    file.write(doc.toJson());
+    file.close();
+    return true;
+}
