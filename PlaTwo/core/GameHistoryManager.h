9930a45069e2d1301efa82f0f9544b2068eaf63c
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-04 14:12:24 +0330

    ADD_FILE: 'GameHistoryManager.h'

---
+++ PlaTwo/core/GameHistoryManager.h
@@ -0,0 +1,13 @@
+#ifndef GAMEHISTORYMANAGER_H
+#define GAMEHISTORYMANAGER_H
+
+#include <QObject>
+
+class GameHistoryManager
+{
+    Q_OBJECT
+public:
+    GameHistoryManager();
+};
+
+#endif // GAMEHISTORYMANAGER_H
