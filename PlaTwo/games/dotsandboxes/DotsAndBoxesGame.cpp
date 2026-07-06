cc155631de35c2c5ec098d17e157de9681816d4f
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-06 21:16:42 +0330

    ADD_FILE: 'DotsAndBoxesGame.h'/'DotsAndBoxesGame.cpp'

---
+++ PlaTwo/games/dotsandboxes/DotsAndBoxesGame.cpp
@@ -0,0 +1,3 @@
+#include "DotsAndBoxesGame.h"
+
+DotsAndBoxesGame::DotsAndBoxesGame() {}
---
+++ PlaTwo/games/dotsandboxes/DotsAndBoxesGame.h
@@ -0,0 +1,11 @@
+#ifndef DOTSANDBOXESGAME_H
+#define DOTSANDBOXESGAME_H
+
+class DotsAndBoxesGame
+{
+    Q_OBJECT
+public:
+    DotsAndBoxesGame();
+};
+
+#endif // DOTSANDBOXESGAME_H
