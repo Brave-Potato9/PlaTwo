b79367ec7dbcfd392efb04fb339c15427c229650
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-15 19:13:12 +0330

    ADD_FILE: 'FanaronaBoard.h'/'FanaronaBoard.cpp'

---
+++ PlaTwo/games/fanarona/FanoronaBoard.cpp
@@ -0,0 +1,3 @@
+#include "FanoronaBoard.h"
+
+FanoronaBoard::FanoronaBoard() {}
---
+++ PlaTwo/games/fanarona/FanoronaBoard.h
@@ -0,0 +1,10 @@
+#ifndef FANORONABOARD_H
+#define FANORONABOARD_H
+
+class FanoronaBoard
+{
+public:
+    FanoronaBoard();
+};
+
+#endif // FANORONABOARD_H
