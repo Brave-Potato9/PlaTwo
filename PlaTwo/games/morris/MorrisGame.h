4248949c0a2b137c282f9d183bd8d90e5411f262
Mohamadreza_Ahmadi <BRAVE.potato9@gmail.com>
2026-07-18 23:37:32 +0330

    ADD_CLASS: 'MorrisGame'

--- PlaTwo/games/morris/MorrisGame.h
+++ PlaTwo/games/morris/MorrisGame.h
@@ -1,13 +1,50 @@
 #ifndef MORRISGAME_H
 #define MORRISGAME_H
 
-#include <QObject>
+#include "../../core/Game.h"
+#include "MorrisBoard.h"
 
-class MorrisGame
+class MorrisGame : public Game
 {
     Q_OBJECT
+
+private:
+    MorrisBoard board;
+
+    bool waitingForRemoval;
+    QList<int> removablePieces;
+
+    //helper_methods
+    bool handlePlacement(const Move& move);
+    bool handleMovement(const Move& move);
+    bool handleRemoval(const Move& move);
+    void checkAndHandleMill(int pos, int player);
+    QList<Move> generatePossibleMoves() const;
+    bool hasAnyValidMove(int player) const;
+    void clearRemovalState();
+    void onGameStart() override;
+    void onGameEnd(const QString& winner) override;
+
 public:
-    MorrisGame();
+    explicit MorrisGame(QObject* parent = nullptr);
+
+    //main_methods
+    void initialize(const GameConfig& config) override;
+    bool makeMove(const Move& move) override;
+    bool isValidMove(const Move& move) const override;
+    QList<Move> getPossibleMoves() const override;
+    bool checkGameOver() override;
+    QString getWinner() const override;
+
+    //supplementry_methods
+    QJsonObject getBoardState() const override;
+    void resetGame() override;
+    QString getGameType() const override ;
+
+    //specialized_method
+    MorrisBoard* getBoard() ;
+    const MorrisBoard* getBoard() const ;
+
 };
 
 #endif // MORRISGAME_H
